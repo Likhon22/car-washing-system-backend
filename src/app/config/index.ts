bbb561dd299c7d3d6dfd2682eec1d1068c4e03eb
Likhon22 <likhonsarker793@gmail.com>
2024-12-07 00:15:50 +0600

    feat:.env updated

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -12,4 +12,5 @@ export default {
   jwt_expiration_time: process.env.JWT_EXPIRATION_TIME,
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_refresh_expiration_time: process.env.JWT_REFRESH_EXPIRATION_TIME,
+  smtp_email_pass: process.env.EMAIL_SMTP_PASSWORD,
 };
